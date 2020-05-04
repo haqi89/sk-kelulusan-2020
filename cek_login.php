930e4d0edb014a7bf4974860c2b98f5e00883c79
kuriyantoadi <melatikuning212@gmail.com>
2020-05-05 02:02:30 +0700

    update 5 mei 2020, sesi 6

--- cek_login.php
+++ cek_login.php
@@ -13,7 +13,7 @@ $nisn = addslashes(trim($_POST['nisn']));
 
 // menyeleksi data admin dengan nisn dan password yang sesuai
 // $data = mysqli_query($koneksi, "select * from login where nisn='$nisn' and password='$password'");
-$data = mysqli_query($koneksi, "select * from login where nisn='$nisn' ");
+$data = mysqli_query($koneksi, "select nisn from login where nisn='$nisn' ");
 
 
 // menghitung jumlah data yang ditemukan
@@ -26,6 +26,7 @@ if ($cek > 0) {
     if ($login['status']=="LULUS") {
         $_SESSION['nisn'] = $nisn;
         $_SESSION['status'] = "LULUS";
+        echo "cek";
         header("location:tampil-siswa.php?nisn=$nisn");
     } else {
         echo "salah1";
